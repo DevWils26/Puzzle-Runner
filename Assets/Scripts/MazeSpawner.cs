6ba40dac01185aa548efd2d8a96c7e9f6704f3b0
DevWils26 <devwils@siue.edu>
2025-04-20 18:28:10 -0500

    Skeletons Spawning

---
+++ Assets/Scripts/MazeSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MazeSpawner : MonoBehaviour
+{
+    public GameObject[] prefabsToSpawn;
+    public int spawnCount = 5;
+    public int mazeWidth = 10;
+    public int mazeHeight = 10;
+    public float cellSize = 1f;
+    public float spawnDelay = 0.1f;
+
+    void Start()
+    {
+        Invoke(nameof(SpawnPrefabs), spawnDelay);
+    }
+
+    void SpawnPrefabs()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            int randX = Random.Range(0, mazeWidth);
+            int randZ = Random.Range(0, mazeHeight);
+            Vector3 spawnPos = new Vector3(randX * cellSize, 0f, randZ * cellSize);
+
+
+            GameObject prefab = prefabsToSpawn[Random.Range(0, prefabsToSpawn.Length)];
+            Instantiate(prefab, spawnPos, Quaternion.identity);
+        }
+    }
+} 
